02ca1f22113cf5cd88ce167d8217762c02b9fa11
Thành Nguyễn <thanhtlu2k44@gmail.com>
2025-06-06 21:31:27 +0700

    Socket

---
+++ LyThuyet/SocketAndRMI/NameRmiClient.java
@@ -0,0 +1,17 @@
+package SocketAndRMI;
+
+import java.rmi.*;
+public class NameRmiClient {
+    public static void main(String args[]) {
+        try {
+            NameService r = (NameService) 
+                           Naming.lookup("rmi://linux02/MyNameServer");
+            int i = r.insert("p1", "tick.ece", 2058);
+            int j = r.search("p1");
+            if (j != -1)
+               System.out.println(r.getHostName(j) +":" + r.getPort(j));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ LyThuyet/SocketAndRMI/NameService.java
@@ -0,0 +1,10 @@
+package SocketAndRMI;
+
+import java.rmi.*;
+public interface NameService extends Remote {
+    public int search(String s) throws RemoteException;
+    public int insert(String s, String hostName, int portNumber)
+            throws RemoteException;
+    public int getPort(int index) throws RemoteException;
+    public String getHostName(int index) throws RemoteException;
+}
